0c72239702cc4ea3f0f61c5bebabc08e6f7f3193
shuirouyan <1141761757@qq.com>
2024-09-19 18:51:34 +0800

    add publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: '/demo-site/',
   devServer: {
     proxy: {
       '/myApi': {
